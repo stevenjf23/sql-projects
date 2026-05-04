d6753993cd71e428c54b815465cbde339e76a64d
stevenjf23 <151056438+stevenjf23@users.noreply.github.com>
2026-05-04 11:30:31 -0600

    Update Clausula GROUP BY.sql

--- 1. DQL/11. Clausula GROUP BY/Clausula GROUP BY.sql
+++ 1. DQL/11. Clausula GROUP BY/Clausula GROUP BY.sql
@@ -12,4 +12,6 @@ SELECT año_lanzamiento, COUNT(serie_id) AS cantidad_de_series FROM Series GROUP
 
 SELECT año_lanzamiento, genero, COUNT(serie_id) AS cantidad_de_series FROM Series GROUP BY año_lanzamiento, genero;
 
+SELECT actor_id, COUNT(serie_id) AS numero_de_series FROM actuaciones GROUP BY actor_id;
+
 SELECT serie_id, AVG(rating_imdb) AS rating_promedio FROM episodios GROUP BY serie_id ORDER BY rating_promedio DESC;
