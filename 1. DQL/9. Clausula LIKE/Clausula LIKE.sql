a70efc7724a704800f81f0fc7fd9beaab7aeaec3
stevenjf23 <151056438+stevenjf23@users.noreply.github.com>
2026-05-04 11:15:43 -0600

    Create Clausula LIKE.sql

---
+++ 1. DQL/9. Clausula LIKE/Clausula LIKE.sql
@@ -0,0 +1,9 @@
+SELECT * FROM Series WHERE titulo LIKE ‘%The%’;
+
+SELECT * FROM Series WHERE titulo LIKE ‘The%’;
+
+SELECT * FROM Series WHERE titulo LIKE ‘%The’;
+
+SELECT * FROM Series WHERE titulo NOT LIKE ‘%The%’;
+
+SELECT titulo FROM Series WHERE titulo LIKE ‘%The%’;
