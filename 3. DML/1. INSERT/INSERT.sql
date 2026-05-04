66ac436a108483f9da4ed84ee97c7f5fc983f073
stevenjf23 <151056438+stevenjf23@users.noreply.github.com>
2026-05-04 16:07:41 -0600

    Update INSERT.sql

--- 3. DML/1. INSERT/INSERT.sql
+++ 3. DML/1. INSERT/INSERT.sql
@@ -1,3 +1,18 @@
 INSERT INTO Departamentos (nombre, ubicacion) VALUES
 ('Recursos Humanos', 'Edificio B'),
 ('Marketing', 'Edificio Central');
+
+INSERT INTO Departamentos (nombre, ubicacion) VALUES
+('Tecnologia', 'Edificio Central'),
+('Recursos Humanos', 'Edificio B'),
+('Marketing', 'Edificio Central'),
+('Ventas', 'Edificio C'),
+('Finanzas', 'Edificio A'),
+('Operaciones', 'Edificio C'),
+('Soporte Tecnico', 'Edificio B'),
+('Investigacion y Desarrollo', 'Edificio Central'),
+('Soporte Cliente', 'Edificio C'),
+('Innovacion', 'Edificio innovacion'),
+('Desarrollo de Producto', 'Edificio D'),
+('Control de Calidad', 'Edificio E'),
+('Logistica', 'Edificio E');
