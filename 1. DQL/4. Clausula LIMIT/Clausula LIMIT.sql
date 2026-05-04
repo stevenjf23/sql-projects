47f97cd601bfdcee12baa6fbb8d72d26cf1e5112
stevenjf23 <151056438+stevenjf23@users.noreply.github.com>
2026-05-04 10:47:17 -0600

    Create Clausula LIMIT.sql

---
+++ 1. DQL/4. Clausula LIMIT/Clausula LIMIT.sql
@@ -0,0 +1,7 @@
+SELECT * FROM Episodios LIMIT 10;
+
+SELECT titulo, duración FROM Episodios ORDER BY duración DESC LIMIT 5;
+
+SELECT nombre FROM Actores LIMIT 5;
+
+SELECT * FROM Episodios LIMIT 10 OFFSET 5;
