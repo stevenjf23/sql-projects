4bfb075f65d51d9e55416e3c2e798277db9cbf96
stevenjf23 <151056438+stevenjf23@users.noreply.github.com>
2026-05-04 11:43:18 -0600

    Create Clausula INNER LEFT RIGHT JOIN.sql

---
+++ 1. DQL/13. Clausula INNER LEFT RIGHT JOIN/Clausula INNER LEFT RIGHT JOIN.sql
@@ -0,0 +1,87 @@
+SELECT * FROM series
+INNER JOIN actuaciones
+ON series.serie_id = actuaciones.serie_id;
+
+SELECT titulo, personaje FROM series
+INNER JOIN actuaciones
+ON series.serie_id = actuaciones.serie_id;
+
+SELECT series.titulo, actuaciones.personaje
+FROM series
+INNER JOIN actuaciones
+ON series.serie_id = actuaciones.serie_id;
+
+SELECT s.titulo, a.personaje
+FROM series AS s
+INNER JOIN actuaciones AS a
+ON s.serie_id = a.serie_id;
+
+
+SELECT s.titulo, a.personaje
+FROM series AS s
+JOIN actuaciones AS a
+ON s.serie_id = a.serie_id;
+
+SELECT s.titulo, a.personaje
+FROM series AS s
+JOIN actuaciones AS a
+ON s.serie_id = a.serie_id
+WHERE s.titulo = 'The crown';
+
+SELECT s.titulo, a.personaje
+FROM series AS s
+JOIN actuaciones AS a
+ON s.serie_id = a.serie_id
+WHERE s.serie_id = 1;
+
+SELECT * FROM netflixdb.series
+INNER JOIN netflixdb.episodios
+ON series.serie_id = episodios.serie_id LIMIT 10;
+
+SELECT s.titulo AS titulo_serie, e.titulo AS titulo_episodio, e.duracion AS duracion FROM netflixdb.series AS s
+INNER JOIN netflixdb.episodios AS e
+ON s.serie_id = e.serie_id
+WHERE s.titulo = 'Stranger Things';
+
+SELECT series.titulo AS 'titulo de la serie',
+episodios.titulo AS 'titulo del episodio'
+FROM netflixdb.series
+LEFT JOIN netflixdb.episodios
+ON series.serie_id = episodios.serie_id
+ORDER BY series.titulo;
+
+SELECT series.titulo AS 'Título de la Serie',
+episodios.titulo AS 'Título del Episodio',
+episodios.rating_imdb AS 'Rating IMDB'
+FROM netflixdb.series
+LEFT JOIN netflixdb.episodios
+ON series.serie_id = episodios.serie_id
+ORDER BY series.titulo ASC;
+
+SELECT series.titulo AS 'Título de la Serie',
+episodios.titulo AS 'Título del Episodio',
+episodios.rating_imdb AS 'Rating IMDB'
+FROM netflixdb.series
+LEFT JOIN netflixdb.episodios
+ON series.serie_id = episodios.serie_id
+WHERE series.titulo = 'Stranger Things'
+ORDER BY episodios.rating_imdb DESC;
+
+SELECT
+series.titulo AS 'Titulo de la serie',
+episodios.titulo AS 'Titulo del episodio'
+FROM Episodios
+RIGHT JOIN series
+ON series.serie_id = episodios.serie_id
+ORDER BY serieS.titulo ASC;
+
+SELECT
+series.titulo AS 'Título de la Serie',
+episodios.titulo AS 'Título del Episodio',
+episodios.duracion AS 'Duración'
+FROM netflixdb.episodios
+RIGHT JOIN netflixdb.series
+ON series.serie_id = episodios.serie_id
+WHERE episodios.duracion > 30
+ORDER BY series.titulo ASC;
+
