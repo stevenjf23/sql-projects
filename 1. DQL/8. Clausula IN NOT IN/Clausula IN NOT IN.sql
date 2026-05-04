cb75524cc9464ed1d3335fe533c2c013e4a5435f
stevenjf23 <151056438+stevenjf23@users.noreply.github.com>
2026-05-04 11:11:22 -0600

    Create Clausula IN NOT IN.sql

---
+++ 1. DQL/8. Clausula IN NOT IN/Clausula IN NOT IN.sql
@@ -0,0 +1,9 @@
+SELECT * FROM Series WHERE genero NOT IN(‘Comedia’);
+
+SELECT * FROM Series WHERE genero IN(‘Comedia’);
+
+SELECT * FROM SERIES where GENERO IN(‘Comedia’, ‘Fantasia’);
+
+SELECT * FROM Series WHERE genero NOT IN (‘Comedia’, ‘Fantasia’);
+
+SELECT * FROM Series WHERE genero NOT IN(‘Drama’, ‘Drama historico’);
