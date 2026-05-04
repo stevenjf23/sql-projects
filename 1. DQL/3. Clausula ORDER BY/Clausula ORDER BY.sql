9dd2e7e8d0e8fd090a24108557abc7773e938e3b
stevenjf23 <151056438+stevenjf23@users.noreply.github.com>
2026-05-04 11:29:21 -0600

    Update Clausula ORDER BY.sql

--- 1. DQL/3. Clausula ORDER BY/Clausula ORDER BY.sql
+++ 1. DQL/3. Clausula ORDER BY/Clausula ORDER BY.sql
@@ -9,3 +9,5 @@ SELECT titulo, duración FROM Episodios ORDER BY titulo DESC;
 SELECT DISTINCT año_lanzamiento FROM Series ORDER BY año_lanzamiento DESC;
 
 SELECT nombre, fecha_nacimiento FROM Actores ORDER BY fecha_nacimiento DESC;
+
+SELECT * FROM episodios ORDER BY duracion DESC;
