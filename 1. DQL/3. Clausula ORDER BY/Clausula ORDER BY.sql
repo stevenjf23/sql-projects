b3e8b0a9c6d66d466941df8f33b8934704bc9733
stevenjf23 <151056438+stevenjf23@users.noreply.github.com>
2026-05-04 10:33:09 -0600

    Create Clausula ORDER BY.sql

---
+++ 1. DQL/3. Clausula ORDER BY/Clausula ORDER BY.sql
@@ -0,0 +1,11 @@
+SELECT titulo, duración FROM Episodios ORDER BY duración;
+
+SELECT titulo, duración FROM Episodios ORDER BY duración ASC;
+
+SELECT titulo, duración FROM Episodios ORDER BY duración DESC
+
+SELECT titulo, duración FROM Episodios ORDER BY titulo DESC;
+
+SELECT DISTINCT año_lanzamiento FROM Series ORDER BY año_lanzamiento DESC;
+
+SELECT nombre, fecha_nacimiento FROM Actores ORDER BY fecha_nacimiento DESC;
