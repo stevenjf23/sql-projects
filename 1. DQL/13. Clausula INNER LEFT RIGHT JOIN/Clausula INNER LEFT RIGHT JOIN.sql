c21fd842e72b17dd1004138ae09f7d4c577f5aa2
stevenjf23 <151056438+stevenjf23@users.noreply.github.com>
2026-05-04 21:16:17 -0600

    Update Clausula INNER LEFT RIGHT JOIN.sql

--- 1. DQL/13. Clausula INNER LEFT RIGHT JOIN/Clausula INNER LEFT RIGHT JOIN.sql
+++ 1. DQL/13. Clausula INNER LEFT RIGHT JOIN/Clausula INNER LEFT RIGHT JOIN.sql
@@ -115,3 +115,31 @@ Proyectos P on A.proyecto_id = P.proyecto_id
 ORDER BY
 E.nombre, E.apellido, A.horas_asignadas DESC;
 
+SELECT
+D.nombre AS 'Nombre Departamento',
+SUM(AP.horas_asignadas) AS 'Total Horas Asignadas'
+FROM
+Departamentos D
+JOIN
+Empleados E ON D.depto_id = E.depto_id
+JOIN
+AsignacionesDeProyectos AP ON E.empleado_id = AP.empleado_id
+GROUP BY
+D.nombre
+ORDER BY
+SUM(AP.horas_asignadas) DESC;
+
+SELECT
+E.nombre AS 'Nombre',
+E.apellido AS 'Apellido',
+SUM(AP.horas_asignadas) AS 'Total Horas Trabajadas',
+RANK() OVER(PARTITION BY E.depto_id ORDER BY SUM(AP.horas_asignadas) DESC) AS 'Ranking Departamental'
+FROM
+Empleados E
+JOIN
+AsignacionesDeProyectos AP ON E.empleado_id = AP.empleado_id
+GROUP BY
+E.empleado_id, E.nombre, E.apellido, E.depto_id
+ORDER BY
+E.depto_id, 'Ranking Departamental';
+
