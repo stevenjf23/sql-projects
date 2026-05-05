4f3736330655009546a292bc5b5b892ff51df336
stevenjf23 <151056438+stevenjf23@users.noreply.github.com>
2026-05-04 21:14:03 -0600

    Update Vistas.sql

--- 4. Programacion SQL/2. Vistas/Vistas.sql
+++ 4. Programacion SQL/2. Vistas/Vistas.sql
@@ -4,3 +4,12 @@ FROM Empleados
 WHERE depto_id = 1;
 
 SELECT * FROM vistaempleadostecnologia;
+
+
+CREATE VIEW VistaEmpleadosProyectos AS
+SELECT E.nombre AS NombreEmpleado, E.apellido AS ApellidoEmpleado, P.nombre AS NombreProyecto
+FROM Empleados E
+JOIN AsignacionesDeProyectos A ON E.empleado_id = A.empleado_id
+JOIN Proyectos P ON A.proyecto_id = P.proyecto_id;
+
+SELECT * FROM vistaempleadosproyectos;
