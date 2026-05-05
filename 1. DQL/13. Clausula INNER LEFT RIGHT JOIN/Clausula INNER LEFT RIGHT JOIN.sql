c65a0c9cb08ca7821c2fad76872b6c023809167f
stevenjf23 <151056438+stevenjf23@users.noreply.github.com>
2026-05-04 21:15:10 -0600

    Update Clausula INNER LEFT RIGHT JOIN.sql

--- 1. DQL/13. Clausula INNER LEFT RIGHT JOIN/Clausula INNER LEFT RIGHT JOIN.sql
+++ 1. DQL/13. Clausula INNER LEFT RIGHT JOIN/Clausula INNER LEFT RIGHT JOIN.sql
@@ -100,3 +100,18 @@ FROM Series
 INNER JOIN episodios ON series.serie_id = episodios.serie_id
 WHERE series.titulo = 'Stranger Things';
 
+
+SELECT
+E.nombre AS 'Nombre Empleado',
+E.apellido AS 'Apellido Empleado',
+P.nombre AS 'Nombre Proyecto',
+A.horas_asignadas AS 'Horas Asignadas'
+FROM
+Empleados E
+JOIN
+AsignacionesDeProyectos A ON E.empleado_id = A.empleado_id
+JOIN
+Proyectos P on A.proyecto_id = P.proyecto_id
+ORDER BY
+E.nombre, E.apellido, A.horas_asignadas DESC;
+
